e3ba29b09ead36e0286086dfcd4f9c3163b07603
Frantisek Gazo <frantisek.gazo@inloop.eu>
2017-03-25 16:49:17 +0100

    Implement p2p communication via Chirp

---
+++ mobile/src/main/java/eu/inoop/ding/DingCore.java
@@ -0,0 +1,146 @@
+package eu.inoop.ding;
+
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.util.Log;
+
+import org.json.JSONObject;
+
+import io.chirp.sdk.CallbackCreate;
+import io.chirp.sdk.CallbackRead;
+import io.chirp.sdk.ChirpSDK;
+import io.chirp.sdk.ChirpSDKListener;
+import io.chirp.sdk.model.Chirp;
+import io.chirp.sdk.model.ChirpError;
+import io.chirp.sdk.model.ChirpProtocolName;
+import io.chirp.sdk.model.ChirpStreamingMode;
+import io.reactivex.BackpressureStrategy;
+import io.reactivex.Flowable;
+import io.reactivex.Observable;
+import io.reactivex.subjects.PublishSubject;
+import io.reactivex.subjects.Subject;
+
+public class DingCore {
+
+
+    private static final String TAG = DingCore.class.getSimpleName();
+
+    @NonNull
+    private final Context mAppContext;
+    @NonNull
+    private final ChirpSDK mChirpSDK;
+    @NonNull
+    private final Subject<JSONObject> mReadSubject = PublishSubject.create();
+
+    private final ChirpSDKListener mChirpSDKListener = new ChirpSDKListener() {
+
+        /*------------------------------------------------------------------------------
+         * onChirpHeard is triggered when a Chirp tone is received.
+         * Obtain the chirp's 10-character identifier with `getIdentifier`.
+         *----------------------------------------------------------------------------*/
+        @Override
+        public void onChirpHeard(Chirp chirp) {
+            Log.d(TAG, "onChirpHeard: " + chirp.getIdentifier());
+
+            /*------------------------------------------------------------------------------
+             * As soon as we hear a chirp, query the API for its associated data.
+             *----------------------------------------------------------------------------*/
+            readChirp(chirp);
+        }
+
+        /*------------------------------------------------------------------------------
+         * onChirpHearStarted is triggered when the beginning of Chirp tone is heard
+         *----------------------------------------------------------------------------*/
+        @Override
+        public void onChirpHearStarted() {
+            Log.d(TAG, "Chirp Hear Started");
+        }
+
+        /*------------------------------------------------------------------------------
+        * onChirpHearFailed is triggered when the beginning of Chirp tone is heard but it
+        * subsequently fails to decode the identifier
+        *----------------------------------------------------------------------------*/
+        @Override
+        public void onChirpHearFailed() {
+            Log.d(TAG, "Chirp Hear Failed");
+        }
+
+        /*------------------------------------------------------------------------------
+         * onChirpError is triggered when an error occurs -- for example,
+         * authentication failure or muted device.
+         *
+         * See the documentation on ChirpError for possible error codes.
+         *----------------------------------------------------------------------------*/
+        @Override
+        public void onChirpError(ChirpError chirpError) {
+            Log.d(TAG, "onChirpError: " + chirpError.getMessage());
+        }
+    };
+
+    public DingCore(@NonNull final Context appContext) {
+        mAppContext = appContext;
+
+        mChirpSDK = new ChirpSDK(mAppContext,
+                mAppContext.getString(R.string.chirp_app_key),
+                mAppContext.getString(R.string.chirp_app_secret));
+        mChirpSDK.setProtocolNamed(ChirpProtocolName.ChirpProtocolNameStandard);
+        mChirpSDK.setListener(mChirpSDKListener);
+    }
+
+    public void resume() {
+        mChirpSDK.start();
+    }
+
+    public void pause() {
+        mChirpSDK.stop();
+    }
+
+    public Observable<JSONObject> startListening() {
+        return mReadSubject;
+    }
+
+    public void send(final JSONObject json) {
+        final Chirp chirpJson = new Chirp(json);
+        mChirpSDK.create(chirpJson, new CallbackCreate() {
+
+            @Override
+            public void onCreateResponse(Chirp chirp) {
+                mChirpSDK.chirp(chirp);
+            }
+
+            @Override
+            public void onCreateError(ChirpError chirpError) {
+                Log.e("chirt error", chirpError.getMessage());
+            }
+        });
+    }
+
+    private void readChirp(Chirp chirp) {
+        /*------------------------------------------------------------------------------
+         * ChirpSDK.read queries the Chirp API for extended data associated with a
+         * given chirp. It requires an internet connection.
+         *----------------------------------------------------------------------------*/
+        mChirpSDK.read(chirp, new CallbackRead() {
+            /*------------------------------------------------------------------------------
+             * The associated data is a single JSON structured object of key-value pairs.
+             * You can define arbitrary nested data structure within this.
+             * Here, we simply retrieve the "text" key.
+             *----------------------------------------------------------------------------*/
+            @Override
+            public void onReadResponse(final Chirp chirp) {
+                final JSONObject jsonData = chirp.getJsonData();
+                Log.d(TAG, "onReadResponse: " + jsonData);
+                mReadSubject.onNext(jsonData);
+            }
+
+            /*------------------------------------------------------------------------------
+             * If an error occurs contacting the Chirp API, generate an error.
+             *----------------------------------------------------------------------------*/
+            @Override
+            public void onReadError(ChirpError chirpError) {
+                Log.d(TAG, "onReadError: " + chirpError.getMessage());
+            }
+        });
+    }
+}
--- mobile/src/main/java/eu/inoop/ding/MainActivity.java
+++ mobile/src/main/java/eu/inoop/ding/MainActivity.java
@@ -1,13 +1,119 @@
 package eu.inoop.ding;
 
-import android.support.v7.app.AppCompatActivity;
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.Random;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.functions.Consumer;
+import io.reactivex.schedulers.Schedulers;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int RESULT_REQUEST_RECORD_AUDIO = 0;
+
+    @Nullable
+    private DingCore mDingCore;
+    @Nullable
+    private Disposable mDisposable;
+
+    @BindView(R.id.txt_value)
+    TextView mTextView;
+    @BindView(R.id.btn_start)
+    Button mStartButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mDingCore = new DingCore(getApplicationContext());
+
+        ButterKnife.bind(this);
+    }
+
+    private boolean doWeHaveRecordAudioPermission() {
+        /*------------------------------------------------------------------------------
+         * Audio permissions are required for Chirp I/O.
+         *----------------------------------------------------------------------------*/
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.RECORD_AUDIO}, RESULT_REQUEST_RECORD_AUDIO);
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        /*------------------------------------------------------------------------------
+         * When foregrounded, start audio processing with chirpSDK.start().
+         * This is required to send or receive chirps.
+         *----------------------------------------------------------------------------*/
+        if (doWeHaveRecordAudioPermission()) {
+            mDingCore.resume();
+
+            mDisposable = mDingCore.startListening()
+                    .subscribeOn(Schedulers.io())
+                    .observeOn(AndroidSchedulers.mainThread())
+                    .subscribe(new Consumer<JSONObject>() {
+                        @Override
+                        public void accept(JSONObject jsonObject) throws Exception {
+                            mTextView.setText(jsonObject.toString());
+                        }
+                    }, new Consumer<Throwable>() {
+                        @Override
+                        public void accept(Throwable throwable) throws Exception {
+                            Log.e("E", throwable.toString());
+                        }
+                    });
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        if (mDisposable != null && !mDisposable.isDisposed()) {
+            mDisposable.dispose();
+        }
+        mDingCore.pause();
+    }
+
+    @OnClick(R.id.btn_start)
+    void onStartClick() {
+        final JSONObject jsonObj = new JSONObject();
+        try {
+            jsonObj.put("price", (new Random().nextFloat() * 1000));
+            jsonObj.put("more", "Pay me!");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        mDingCore.send(jsonObj);
+
+        mTextView.setText("Sent: " + jsonObj);
+    }
+
+    @OnClick(R.id.btn_reset)
+    void onResetClick() {
+        mTextView.setText("");
     }
 }
