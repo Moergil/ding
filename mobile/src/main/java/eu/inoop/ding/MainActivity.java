ff7b38d9fabce533766e6009088f0f67b92a2b3c
Matej Istenik <matej.istenik@inloop.eu>
2017-03-25 09:01:27 +0100

    Initial commit

---
+++ mobile/src/main/java/eu/inoop/ding/MainActivity.java
@@ -0,0 +1,13 @@
+package eu.inoop.ding;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
